5eb4a4452efada56cad2d6469eddcb44ce377067
shihua.jiang <abc@123>
2022-04-26 08:53:15 +0800

    feat: 更新ax6配置

--- ax6/diy-part2-5.15-immortalwrt.sh
+++ ax6/diy-part2-5.15-immortalwrt.sh
@@ -6,7 +6,7 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2-5.15-imoutowrt.sh
+# File name: diy-part2-5.15-immortalwrt.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
