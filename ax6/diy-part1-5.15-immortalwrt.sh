ddc6d0e9511cdb6fd7c459abb0214f2dc3de6f80
shihua.jiang <abc@123>
2022-05-25 14:01:00 +0800

    feat: 增加adguardhome插件

--- ax6/diy-part1-5.15-immortalwrt.sh
+++ ax6/diy-part1-5.15-immortalwrt.sh
@@ -19,4 +19,6 @@ git revert --no-edit 7ae8b0afc13b76e3ed4c2f14baee2d0317a76517
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
