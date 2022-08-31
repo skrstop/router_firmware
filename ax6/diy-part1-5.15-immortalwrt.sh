183635f526dd4a3975553f2d64aeea6216fa548d
jphoebe <abc@123>
2022-08-31 09:29:24 +0800

    feat: 适配新源码

--- ax6/diy-part1-5.15-immortalwrt.sh
+++ ax6/diy-part1-5.15-immortalwrt.sh
@@ -17,6 +17,15 @@
 #git config --global user.name "hnyyghk"
 #git revert --no-edit 7ae8b0afc13b76e3ed4c2f14baee2d0317a76517
 
+echo '' > feeds.conf.default
+echo 'src-git-full packages https://github.com/immortalwrt/packages.git' >> feeds.conf.default
+echo 'src-git-full luci https://github.com/immortalwrt/luci.git' >> feeds.conf.default
+echo 'src-git-full routing https://github.com/openwrt/routing.git' >> feeds.conf.default
+echo 'src-git-full telephony https://github.com/openwrt/telephony.git' >> feeds.conf.default
+echo '#src-git-full video https://github.com/openwrt/video.git' >> feeds.conf.default
+echo '#src-git-full targets https://github.com/openwrt/targets.git' >> feeds.conf.default
+echo '#src-git-full oldpackages http://git.openwrt.org/packages.git' >> feeds.conf.default
+
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
