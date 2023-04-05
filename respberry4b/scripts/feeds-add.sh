08de61f80bb842518ae16b6da0972f66fabf32c2
Brenda Taylor <4086824@qq.com>
2023-04-05 13:02:00 +0800

    Update feeds-add.sh

--- respberry4b/scripts/feeds-add.sh
+++ respberry4b/scripts/feeds-add.sh
@@ -11,6 +11,6 @@ echo '#src-link custom /usr/src/openwrt/custom-feed' >> feeds.conf.default
 echo '#src-git-full oldpackages http://git.openwrt.org/packages.git' >> feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
