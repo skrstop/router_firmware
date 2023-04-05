c0e9d3bf7627eb52b7d2797b139a53cfc19ce839
Brenda Taylor <4086824@qq.com>
2023-04-05 13:27:22 +0800

    Update feeds-add.sh

--- respberry4b/scripts/feeds-add.sh
+++ respberry4b/scripts/feeds-add.sh
@@ -4,11 +4,11 @@ echo 'src-git-full packages https://github.com/immortalwrt/packages.git' >> feed
 echo 'src-git-full luci https://github.com/immortalwrt/luci.git' >> feeds.conf.default
 echo 'src-git-full routing https://github.com/openwrt/routing.git' >> feeds.conf.default
 echo 'src-git-full telephony https://github.com/openwrt/telephony.git' >> feeds.conf.default
-echo '#src-git-full video https://github.com/openwrt/video.git' >> feeds.conf.default
-echo '#src-git-full targets https://github.com/openwrt/targets.git' >> feeds.conf.default
-echo '#src-git-full oldpackages http://git.openwrt.org/packages.git' >> feeds.conf.default
-echo '#src-link custom /usr/src/openwrt/custom-feed' >> feeds.conf.default
-echo '#src-git-full oldpackages http://git.openwrt.org/packages.git' >> feeds.conf.default
+# echo '#src-git-full video https://github.com/openwrt/video.git' >> feeds.conf.default
+# echo '#src-git-full targets https://github.com/openwrt/targets.git' >> feeds.conf.default
+# echo '#src-git-full oldpackages http://git.openwrt.org/packages.git' >> feeds.conf.default
+# echo '#src-link custom /usr/src/openwrt/custom-feed' >> feeds.conf.default
+# echo '#src-git-full oldpackages http://git.openwrt.org/packages.git' >> feeds.conf.default
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
