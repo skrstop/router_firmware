14beb55efe5224624dff12b66b215e47eea75b0d
shihua.jiang <4086824@qq.com>
2023-07-24 15:33:40 +0800

    修改任务名

--- respberry4b/scripts/fix_bug.sh
+++ respberry4b/scripts/fix_bug.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-if [ ! -d $GITHUB_WORKSPACE/workspace/openwrt/build_dir/host/python ];then
-  echo "python文件夹不存在存在, 创建软链接"
-  ln -s /usr/bin/python $GITHUB_WORKSPACE/workspace/openwrt/build_dir/host/python
+if [ ! -d $GITHUB_WORKSPACE/workspace/openwrt/staging_dir/host/bin/python ];then
+  echo "staging_dir/host/bin/python文件夹不存在存在, 创建软链接"
   ln -s /usr/bin/python $GITHUB_WORKSPACE/workspace/openwrt/staging_dir/host/bin/python
-  $GITHUB_WORKSPACE/workspace/openwrt/build_dir/host/python --version
   $GITHUB_WORKSPACE/workspace/openwrt/staging_dir/host/bin/python --version
 fi
 
-if [ ! -d $GITHUB_WORKSPACE/workspace/openwrt/build_dir/host/python3 ];then
-  echo "python3文件夹不存在存在, 创建软链接"
-  ln -s /usr/bin/python $GITHUB_WORKSPACE/workspace/openwrt/build_dir/host/python3
+if [ ! -d $GITHUB_WORKSPACE/workspace/openwrt/staging_dir/host/bin/python3 ];then
+  echo "staging_dir/host/bin/python3文件夹不存在存在, 创建软链接"
   ln -s /usr/bin/python $GITHUB_WORKSPACE/workspace/openwrt/staging_dir/host/bin/python3
-  $GITHUB_WORKSPACE/workspace/openwrt/build_dir/host/python3 --version
   $GITHUB_WORKSPACE/workspace/openwrt/staging_dir/host/bin/python3 --version
 fi
