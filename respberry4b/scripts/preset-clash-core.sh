934d5812151ddcc3e2e09df46f5042ab8abbb9a0
shihua.jiang <4086824@qq.com>
2023-07-24 09:56:36 +0800

    feat: 更新编译脚本

--- respberry4b/scripts/preset-clash-core.sh
+++ respberry4b/scripts/preset-clash-core.sh
@@ -9,25 +9,36 @@
 # Blog: https://mlapp.cn
 #=================================================
 
-mkdir -p files/etc/openclash/core
-# openclash 的 dev内核
-# CLASH_DEV_URL="https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-${1}.tar.gz"
-CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz"
-# openclash 的 TUN内核
-# CLASH_TUN_VERSION=$(curl -sL https://github.com/vernesong/OpenClash/raw/core/master/core_version | head -n 2 | tail -n 1)
-# CLASH_TUN_URL="https://github.com/vernesong/OpenClash/raw/core/master/premium/clash-linux-${1}-$CLASH_TUN_VERSION.gz"
-CLASH_TUN_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-arm64-2023.06.30.gz"
-# openclash 的 Meta内核版本
-# CLASH_META_URL="https://github.com/vernesong/OpenClash/raw/core/master/meta/clash-linux-${1}.tar.gz"
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
 
+# 预置openclash内核
+mkdir -p files/etc/openclash/core
+
+
+# d大 的 dev内核
+CLASH_DEV_URL=$(curl -sL https://api.github.com/repos/Dreamacro/clash/releases/latest | grep /clash-linux-arm64-v1 | awk -F '"' '{print $4}')
+# d大 的 premium内核
+CLASH_TUN_URL=$(curl -sL https://api.github.com/repos/Dreamacro/clash/releases/tags/premium | grep /clash-linux-arm64-2 | awk -F '"' '{print $4}' | head -n 1)
+# Meta内核版本
+CLASH_META_URL=$(curl -sL https://api.github.com/repos/MetaCubeX/Clash.Meta/releases/tags/Prerelease-Alpha | grep /clash.meta-linux-arm64-alpha | awk -F '"' '{print $4}' | head -n 1)
+
+# CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-arm64.tar.gz"
+# CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/core-lateset/premium | grep download_url | grep $1 | awk -F '"' '{print $4}')
+# CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/meta/clash-linux-${1}.tar.gz"
+
+wget -qO- $CLASH_DEV_URL | gunzip -c > files/etc/openclash/core/clash
+wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
+wget -qO- $CLASH_META_URL | gunzip -c > files/etc/openclash/core/clash_meta
+# 给内核权限
+chmod +x files/etc/openclash/core/clash*
+
+# meta 要GeoIP.dat 和 GeoSite.dat
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
 
-wget -qO- $CLASH_DEV_URL | tar xOvz >files/etc/openclash/core/clash
-wget -qO- $CLASH_TUN_URL | gunzip -c >files/etc/openclash/core/clash_tun
-wget -qO- $CLASH_META_URL | tar xOvz >files/etc/openclash/core/clash_meta
-wget -qO- $GEOIP_URL >files/etc/openclash/GeoIP.dat
-wget -qO- $GEOSITE_URL >files/etc/openclash/GeoSite.dat
-
-chmod +x files/etc/openclash/core/clash*
+# Country.mmdb
+COUNTRY_LITE_URL=https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/lite/Country.mmdb
+# COUNTRY_FULL_URL=https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb
+wget -qO- $COUNTRY_LITE_URL > files/etc/openclash/Country.mmdb
+# wget -qO- $COUNTRY_FULL_URL > files/etc/openclash/Country.mmdb
